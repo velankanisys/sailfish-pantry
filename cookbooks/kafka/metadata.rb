16543c5b535035b4d74c29af41b72cdf53f5479f
Murali Raju <murali.raju@appliv.com>
2012-11-25 12:04:49 -0500

    Meta data updates

--- cookbooks/kafka/metadata.rb
+++ cookbooks/kafka/metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Velankani Information Systems, Inc"
+maintainer_email "eng@velankani.net"
 license          "All rights reserved"
 description      "Installs/Configures kafka"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.0.1"
